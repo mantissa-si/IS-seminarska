d49feb2051b987710810282c9a88a662fb06a197
Tim Rekelj <tim.rekelj@proton.me>
2023-01-08 15:00:32 +0100

    swagger fixes

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     build: {
-        chunkSizeWarningLimit: 1000,
+        chunkSizeWarningLimit: 1600,
     },
     plugins: [
-        laravel(
+        laravel([
+            'resources/js/swagger.js',
+        ],
         {
             input: [ 
                 'resources/js/app.jsx',
