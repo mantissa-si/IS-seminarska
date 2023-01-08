373b8bf23020362d9ac10d0ae12b5439eaaaca39
Tim Rekelj <tim.rekelj@proton.me>
2023-01-08 14:41:10 +0100

    swagger fix

--- resources/js/swagger.js
+++ resources/js/swagger.js
@@ -2,5 +2,5 @@ import SwaggerUI from 'swagger-ui'
 import 'swagger-ui/dist/swagger-ui.css';
 SwaggerUI({
     dom_id: '#swagger-api',
-    url: '/api.yaml',
+    url: 'https://testing.andrazadamic.si/api.yaml',
 });
