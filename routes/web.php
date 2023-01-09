8fa59768e0a2dcb6b17675a6337e4747f231edbe
Andraž Adamič <andraz.adamic@gmail.com>
2023-01-09 03:10:10 +0100

    :(

--- routes/web.php
+++ routes/web.php
@@ -18,18 +18,10 @@ use App\Http\Controllers\TeamController;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-})->middleware('loggedin')->name('home');
 
 Route::view('/swagger', 'swagger');
 
-Route::get('/dashboard', function () {
+Route::get('/', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
